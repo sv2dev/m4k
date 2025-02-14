30002dfeeb42b352ced9a3a4d4252a4ce331e821
Sven Rogge <sreglitzki@gmail.com>
2025-02-14 08:51:11 +0100

    Fix test script

--- test.ts
+++ test.ts
@@ -1,11 +1,30 @@
-const query = new URLSearchParams({
-  format: "mp4",
-  // videoCodec: "libsvtav1",
-});
+import { streamParts } from "@sv2dev/multipart-stream";
 
-const res = await fetch(`http://localhost:3000/videos/process?${query}`, {
-  method: "POST",
-  body: Bun.file("fixtures/video.mp4"),
-});
+const res = await fetch(
+  `http://localhost:3000/videos/process?${new URLSearchParams({
+    format: "mp4",
+    videoCodec: "libsvtav1",
+  })}`,
+  {
+    method: "POST",
+    body: Bun.file("fixtures/video.mp4"),
+  }
+);
 
-await Bun.write("test.mp4", res);
+const [, video] = await Array.fromAsync(streamParts(res));
+
+await Bun.write("test.mp4", await video.bytes());
+
+const res2 = await fetch(
+  `http://localhost:3000/images/process?${new URLSearchParams({
+    format: "avif",
+  })}`,
+  {
+    method: "POST",
+    body: Bun.file("fixtures/image.jpeg"),
+  }
+);
+
+const [, image] = await Array.fromAsync(streamParts(res2));
+
+await Bun.write("test.avif", await image.bytes());
