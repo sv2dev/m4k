c2d4f130967dbfd82f61dc5ee92e57c35b2c0e44
Sven Rogge <mail@sven-rogge.com>
2026-02-22 22:43:37 +0100

    Export mimeTypes from the util/mime module in the core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,9 +5,10 @@ export {
   type ProcessingError,
   type Progress,
   type QueuePosition,
-  type VideoOptions,
+  type VideoOptions
 } from "@m4k/common";
 export { audioQueue, processAudio } from "./audio/audio-processor";
 export { imageQueue, processImage } from "./images/image-processor";
 export { getExtension } from "./util/ffmpeg-processor";
+export { mimeTypes } from "./util/mime";
 export { processVideo, videoQueue } from "./videos/video-processor";
